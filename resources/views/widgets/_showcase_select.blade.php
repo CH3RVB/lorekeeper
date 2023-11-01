7237adb88a38f5aa7c5782076276c8b4d7f14b1f
jam <ch3rvbcreations@gmail.com>
2023-11-01 16:59:47 -0400

    fix toggle for real this time + better mobile view

--- resources/views/widgets/_showcase_select.blade.php
+++ resources/views/widgets/_showcase_select.blade.php
@@ -1,36 +1,38 @@
-{!! Form::hidden('showcase_id', $showcase->id) !!}
-<table class="table table-sm" id="stockTable">
-    <thead>
-        <tr>
-            <th width="15%">Item</th>
-            <th width="15%">Visible?</th>
-            <th width="20%">Removal Quantity</th>
-        </tr>
-    </thead>
-    <tbody id="stockTableBody">
-        @foreach ($showcase->stock->where('quantity', '>', 0) as $stock)
-            <tr class="stock-row">
-                {!! Form::hidden('stock_id[]', $stock->id) !!}
-                <td>
-                    @if (isset($stock->item->image_url))
-                        <img class="small-icon" src="{{ $stock->item->image_url }}" alt="{{ $stock->item->name }}">
-                    @endif
-                    {!! $stock->item->name !!}
-                    @if (!$stock->is_visible)
-                        <i class="fas fa-eye-slash mr-1"></i>
-                    @endif
-                </td>
-                <td>{!! Form::checkbox('is_visible[]', 1, $stock->is_visible ?? 1, [
+<div id="stockTable">
+    <div class="row border-bottom">
+        <div class="col-6 col-md-3">Item</div>
+        <div class="col-6 col-md-3 order-3 order-md-2">Visible?</div>
+        <div class="col-6 col-md-3">Removal Quantity</div>
+    </div>
+
+    @foreach ($showcase->stock->where('quantity', '>', 0) as $stock)
+        <div class="row flex-wrap border-bottom" id="stockTableBody">
+            {!! Form::hidden('stock_id[]', $stock->id) !!}
+
+            <div class="col-6 col-md-3">
+                @if (isset($stock->item->image_url))
+                    <img class="small-icon" src="{{ $stock->item->image_url }}" alt="{{ $stock->item->name }}">
+                @endif
+                {!! $stock->item->name !!} - {{ $stock->stock_type }}
+                @if (!$stock->is_visible)
+                    <i class="fas fa-eye-slash mr-1"></i>
+                @endif
+            </div>
+
+            <div class="col-6 col-md-3 order-3 order-md-2">
+                {!! Form::checkbox('is_visible[' . $stock->id . ']', 1, $stock->is_visible ?? 1, [
                     'class' => 'form-check-input',
                     'data-toggle' => 'toggle',
                 ]) !!}
-                </td>
-                <td class="col-5">{!! Form::selectRange('quantity[]', 0, $stock->quantity, 0, [
+            </div>
+
+            <div class="col-6 col-md-3">
+                {!! Form::selectRange('quantity[]', 0, $stock->quantity, 0, [
                     'class' => 'quantity-select',
                     'type' => 'number',
                     'style' => 'min-width:40px;',
-                ]) !!} /{{ $stock->quantity }} </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+                ]) !!} /{{ $stock->quantity }}
+            </div>
+        </div>
+    @endforeach
+</div>
