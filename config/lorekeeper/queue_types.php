e0e748354a835717df90b6956ff537263410234f
jam <ch3rvbcreations@gmail.com>
2025-10-16 09:46:10 -0400

    :bug: Fix config

--- config/lorekeeper/queue_types.php
+++ config/lorekeeper/queue_types.php
@@ -10,10 +10,17 @@
 
 return [
 
-    'vanilla' => [
+    'vanilla'   => [
         'name'             => 'Vanilla',
         'item_consume'     => false,
         'character_submit' => false,
         'image_upload'     => false,
+    ],
+
+    'char_icon' => [
+        'name'             => 'Character Icon',
+        'item_consume'     => false,
+        'character_submit' => true,
+        'image_upload'     => true,
     ],
 ];
